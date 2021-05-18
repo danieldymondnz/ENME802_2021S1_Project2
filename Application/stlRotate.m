18ea0daa9353c5422743eec1e2ad5720dd4adae0
Rospat <66628913+Rospat@users.noreply.github.com>
2021-05-19 10:41:06 +1200

    more testing

--- Application/stlRotate.m
+++ Application/stlRotate.m
@@ -1,5 +1,10 @@
-function stl = stlRotate(stl)
+function stlRotated = stlRotate(stl)
 X = stl.Points(:,1);
 Y = stl.Points(:,2);
+Z = stl.Points(:,3);
+
+ 
+stlRotated.Points = [Z,X,Y];
+stlRotated.ConnectivityList = stl.ConnectivityList;
 
 end
