2dc7bb0d6dff0f248332c49b47f20ff82daf040f
Daniel Dymond <daniel@danieldymond.nz>
2021-05-23 09:43:22 +1200

    Added options to translate and autoplace STL File

--- Application/stlTransform.m
+++ Application/stlTransform.m
@@ -1,6 +1,6 @@
-function transformedSTL = stlTransform(stlToTransform, thetaX, thetaY, thetaZ)
+function transformedSTL = stlTransform(stlToTransform, thetaX, thetaY, thetaZ, autoPlace, dX, dY, dZ)
 
-    % Credit: Code derrived from Khan Academy
+    % Credit: Rotation Code derrived from Khan Academy
     % https://www.khanacademy.org/computing/computer-programming/programming-games-visualizations/programming-3d-shapes/a/rotating-3d-shapes
 
     % Copy information from the STL
@@ -28,16 +28,35 @@ function transformedSTL = stlTransform(stlToTransform, thetaX, thetaY, thetaZ)
         stlPoints = rotateZ(stlPoints, thetaZ);
     end
     
-    % Check X,Y,Z Coordinates and fix model such that it sits on the plane
-    for i = 1:3
-        minOnAxis = min(stlPoints(:,i));
-        if minOnAxis ~= 0
-            stlPoints(:,i) = stlPoints(:,i) - minOnAxis;
-        end
+    % Auto-place coordinates if requested
+    if (autoPlace)
+        stlPoints = autoPlaceCoordinates(stlPoints);
+    elseif (dX ~= 0 || dy ~= 0 || dz ~= 0)
+        stlPoints = translateCoordinates(stlPoints, dX, dY, dZ);
     end
     
     % Create new triangulation object to return 
     transformedSTL = triangulation(stlConList, stlPoints);
+
+end
+
+function coordinatesToTranslate = translateCoordinates(coordinatesToTranslate, dX, dY, dZ)
+
+    coordinatesToTranslate(:,1) = coordinatesToTranslate(:,1) + dX;
+    coordinatesToTranslate(:,2) = coordinatesToTranslate(:,2) + dY;
+    coordinatesToTranslate(:,3) = coordinatesToTranslate(:,3) + dZ;
+
+end
+
+function coordinatesToAutoPlace = autoPlaceCoordinates(coordinatesToAutoPlace)
+
+    % Check X,Y,Z Coordinates and fix model such that it sits on the plane
+    for i = 1:3
+        minOnAxis = min(coordinatesToAutoPlace(:,i));
+        if minOnAxis ~= 0
+            coordinatesToAutoPlace(:,i) = coordinatesToAutoPlace(:,i) - minOnAxis;
+        end
+    end
 
 end
 
