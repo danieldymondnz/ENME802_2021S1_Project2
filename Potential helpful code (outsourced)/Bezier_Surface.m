d9804f4e8c38375ef89b50771755ae5326880222
Daniel Dymond <daniel@danieldymond.nz>
2021-05-24 11:03:52 +1200

    Optimisations to Bezier Surface Code
    Auto-detect n,m and fixed meshPlot transpose issue.

--- Potential helpful code (outsourced)/Bezier_Surface.m
+++ Potential helpful code (outsourced)/Bezier_Surface.m
@@ -1,6 +1,13 @@
 clear all 
-n = 4;              %For U matrix
-m = 5;              %For W matrix *note that these do not need to be equal in reality 
+
+B = [0 10 20 0;         %Position verxtor of each point (x,y)
+    10 120 60 10;
+    10 90 110 10; 
+    0 40 30 0];
+
+n = height(B);      %For U matrix
+m = length(B);      %For W matrix *note that these do not need to be equal in reality 
+
 via = 20;           %Divisions
 
 lx = 0;             %Knowing coord. points of x - lower and upper values
@@ -9,10 +16,7 @@ ux = 10;
 ly= 0;              %Knowing coord points of y - lower and upper values
 uy = 10; 
 
-B = [0 10 20 0;     %Position verxtor of each point 
-    10 120 60 10;
-    10 90 110 10; 
-    0 40 30 0];
+% Parametric Relationship
 t = 0:1/(via-1):1;
 
 for j = 1:via
@@ -22,7 +26,7 @@ for j = 1:via
 end 
 
 for j = 1:via
-    for i = 1:n
+    for i = 1:m
         W(j,i) = t(j)^(m-i);
     end 
 end 
@@ -49,7 +53,7 @@ for i = 1:m
     end 
 end
 
-Z = U*N*B.*M'*W'                 %General Bezier surface equation 
+Z = U*N*B*M'*W'                 %General Bezier surface equation 
 rx = lx:(ux-lx)/(via-1):ux;     %Taking the lowest and highest "boundaries" and dividing that section by the number of divisions
 ry = ly:(uy-ly)/(via-1):uy;
 [X,Y] = meshgrid(rx,ry)
@@ -59,5 +63,5 @@ mesh(X,Y,Z); hold on
 
 X1 = lx:(ux-lx)/(n-1):ux;
 Y1 = ly:(uy-ly)/(m-1):uy;
-mesh(X1,Y1,B); hold on; hidden off
+mesh(X1,Y1,B.'); hold on; hidden off
 surf(X,Y,Z); xlabel('X-axis'); ylabel('Y-axis'); zlabel('Z-axis');
