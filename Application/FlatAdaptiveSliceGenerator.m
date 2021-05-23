33c839a5674f03c794212bb0db880fe21ccecab5
Daniel Dymond <daniel@danieldymond.nz>
2021-05-24 11:35:45 +1200

    Added Function Override for generateSliceHeights
    Currently adopts identical code to the uniform slicer - test to determine overrides in MATLAB.

--- Application/FlatAdaptiveSliceGenerator.m
+++ Application/FlatAdaptiveSliceGenerator.m
@@ -4,6 +4,9 @@ classdef FlatAdaptiveSliceGenerator < FlatUniformSliceGenerator
     
     properties (SetAccess = protected)
         
+        minThickness = 0.05;
+        maxThickness = 0.25;
+        
     end
     
     methods (Access = public)
@@ -11,6 +14,33 @@ classdef FlatAdaptiveSliceGenerator < FlatUniformSliceGenerator
         % Create an instance using Triangulation Data
         function obj = FlatAdaptiveSliceGenerator(data, preferedThickness)
             obj = obj@FlatUniformSliceGenerator(data, preferedThickness);
+        end
+    end
+    
+    methods (Access = protected)
+        
+        % Overrides the Uniform Slicer
+        function sliceHeights = generateSliceHeights(obj)
+        % Generates the Z values for which each of the flat layer slices
+        % will be generated at.
+        
+            % Get the maximum Z height
+            maxZ = max(obj.points(:, 3));
+            
+            % Generate a list of all the slice heights
+            sliceHeights = zeros(0);
+            currZ = 0;
+            
+            while currZ < maxZ
+               
+                sliceHeights = [sliceHeights; currZ];
+                currZ = currZ + obj.sliceThickness;
+                
+            end
+            
+            % Add the top layer
+            sliceHeights = [sliceHeights; maxZ];
+            
         end
         
     end
