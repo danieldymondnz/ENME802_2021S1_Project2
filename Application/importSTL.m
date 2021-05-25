c5595cf8c4342c8d53d278e05be58d43ca63b554
Rospat <66628913+Rospat@users.noreply.github.com>
2021-05-25 23:09:12 +1200

    can handle ASCII stl

--- Application/importSTL.m
+++ Application/importSTL.m
@@ -5,40 +5,164 @@ fid = fopen(fileLocation);
 
 % data = fread(fid); 
 % header = data(1:80);
+
+% headerLength = fread(fid,80,'int8');
+% title2 = convertCharsToStrings( native2unicode(headerLength,'ascii') ) ;
+
+% look for endSolid at the end, and if it exists then end the code.
+
+frewind(fid); % Go back to top of fid binary.
 header = fread(fid,80,'int8');
-title = native2unicode(header,'ascii');
+title = convertCharsToStrings( native2unicode(header,'ascii') );
 
-% nfaces = data(81);
-nfaces = fread(fid,1,'int32');
-
-nvert = 3*nfaces; % number of vertices
-
-points = zeros(nvert,3);
-connectivityList = zeros(nfaces,3);
-
-
-for i = 1:nfaces 
-    data = fread(fid,12,'float');
-    % first 12 bytes or, first 3 items are normal vector.
-    % next 12 are x,y,z coordinates of point 1, 2 and 3.
-    
-    % Check here if x y z coordinates already exists.
-    
-    point1 = data(4:6);
-    point2 = data(7:9);
-    point3 = data(10:12);
-    
-    points(i*3-2,:) = point1; % coordinates of point 1
-    points(i*3-1,:) = point2; % %coordinates of point 2
-    points(i*3,:) = point3; % %coordinates of point 3
-    
-    connectivityList(i,:) = [3*i-2 3*i-1 3*i]; % connectivity list pattern
-    fread(fid,1,'int16'); % skip to Attribute byte count 
+if contains(title,"facet normal")
+%     error('ERROR: Please provide an STL file that is encoded in Binary not ASCII. MATLAB can not handle the amount of bytes in ASCII STLs''')
+%     return 
+    format = "ascii";
+else
+    format = "binary";
 end
 
-% obj = triangulation(connectivityList,points);
-fclose(fid);
-obj = triangulation(connectivityList,points);
+if strcmp(format,"binary")
+    
+    frewind(fid); % Go back to top of fid binary.
+    header = fread(fid,80,'int8');
+    title = convertCharsToStrings( native2unicode(header,'ascii') );
+
+    % nfaces = data(81);
+    nfaces = fread(fid,1,'int32');
+
+    nvert = 3*nfaces; % number of vertices
+
+    points = zeros(nvert,3);
+    connectivityList = zeros(nfaces,3);
+
+
+    for i = 1:nfaces 
+        data = fread(fid,12,'float');
+        % first 12 bytes or, first 3 items are normal vector.
+        % next 12 are x,y,z coordinates of point 1, 2 and 3.
+
+        point1 = data(4:6);
+        point2 = data(7:9);
+        point3 = data(10:12);
+
+        index1 = 3*i-2;
+        index2 = 3*i-1;
+        index3 = 3*i;
+    %     connectivityList(i,:) = [3*i-2 3*i-1 3*i]; % connectivity list pattern
+
+        % Check here if x y z coordinates already exists.
+        if( sum( ismember(points,[point1(1,1) point1(2,1) point1(3,1)],'rows') ) >=1)
+            index1 = find(ismember(points,[point1(1,1) point1(2,1) point1(3,1)],'rows'));
+            index1 = index1(1);
+        else
+            points(i*3-2,:) = point1; % coordinates of point 1
+        end
+
+
+        if( sum( ismember(points,[point2(1,1) point2(2,1) point2(3,1)],'rows') ) >=1)
+            index2 = find(ismember(points,[point2(1,1) point2(2,1) point2(3,1)],'rows'));
+            index2 = index2(1);
+        else
+            points(i*3-1,:) = point2; % %coordinates of point 2
+        end
+
+
+        if( sum( ismember(points,[point3(1,1) point3(2,1) point3(3,1)],'rows') ) >=1)
+            index3 = find(ismember(points,[point3(1,1) point3(2,1) point3(3,1)],'rows'));
+            index3 = index3(1);
+        else
+            points(i*3,:) = point3; % %coordinates of point 3
+        end
+
+
+
+
+
+        connectivityList(i,:) = [index1 index2 index3]; % connectivity list pattern
+        fread(fid,1,'int16'); % skip to Attribute byte count 
+    end
+
+
+    % obj = triangulation(connectivityList,points);
+    fclose(fid);
+    obj = triangulation(connectivityList,points);
+    
+    % ASCII PART
+else
+    frewind(fid);
+%     textscan(fid,'%s','delimiter','\n');
+    data = fread(fid,'int8');
+    data = convertCharsToStrings( native2unicode(data,'ascii'));
+%     data = replace(data,'↵','');
+    data = splitlines(data);
+    
+    title = data(1);
+    
+    % nfaces = data(81);
+    nfaces = sum(contains(data,"endfacet"));
+
+    nvert = 3*nfaces; % number of vertices
+
+    points = zeros(nvert,3);
+    connectivityList = zeros(nfaces,3);
+    
+    
+    for i=1:sum(contains(data,"endfacet"))
+        temp = data((i*7-5):(i*7+1));
+        
+        temp1 = split( strtrim(temp(3)), ' ' );
+%         point1 = [str2double(temp1(2));str2double(temp1(3));str2double(temp1(4))];
+        point1 = str2double(temp1(2:end));
+        temp2 = split( strtrim(temp(4)), ' ' );
+%         point2 = [str2double(temp2(2));str2double(temp2(3));str2double(temp2(4))];
+        point2 = str2double(temp2(2:end));
+        temp3 = split( strtrim(temp(5)), ' ' );
+%         point3 = [str2double(temp3(2));str2double(temp3(3));str2double(temp3(4))];
+        point3 = str2double(temp3(2:end));
+        
+        index1 = 3*i-2;
+        index2 = 3*i-1;
+        index3 = 3*i;
+        
+        % Check here if x y z coordinates already exists.
+        if( sum( ismember(points,[point1(1,1) point1(2,1) point1(3,1)],'rows') ) >=1)
+            index1 = find(ismember(points,[point1(1,1) point1(2,1) point1(3,1)],'rows'));
+            index1 = index1(1);
+        else
+            points(i*3-2,:) = point1; % coordinates of point 1
+        end
+
+
+        if( sum( ismember(points,[point2(1,1) point2(2,1) point2(3,1)],'rows') ) >=1)
+            index2 = find(ismember(points,[point2(1,1) point2(2,1) point2(3,1)],'rows'));
+            index2 = index2(1);
+        else
+            points(i*3-1,:) = point2; % %coordinates of point 2
+        end
+
+
+        if( sum( ismember(points,[point3(1,1) point3(2,1) point3(3,1)],'rows') ) >=1)
+            index3 = find(ismember(points,[point3(1,1) point3(2,1) point3(3,1)],'rows'));
+            index3 = index3(1);
+        else
+            points(i*3,:) = point3; % %coordinates of point 3
+        end
+
+
+
+
+
+        connectivityList(i,:) = [index1 index2 index3]; % connectivity list pattern
+        
+    end
+    
+    fclose(fid);
+    obj = triangulation(connectivityList,points);
+end
+
 
 end
+
 
