64eb90cdc6028e7419ca2e5f65c54634c506a613
Daniel Dymond <daniel@danieldymond.nz>
2021-05-26 21:01:22 +1200

    Moved File

--- Application/setPlaneEquation.m
+++
@@ -1,5 +0,0 @@
-function planeEquation = setPlaneEquation(xCoefficient,xModifier,yCoefficient,yModifier,zCoefficient,zModifier)
-syms x y z;
-
-planeEquation = xCoefficient*x^xModifier + yCoefficient*y^yModifier + zCoefficient*z^zModifier;
-end
