53aa20649b806b61b67557a6207f4b1acbcb38b4
Rospat <66628913+Rospat@users.noreply.github.com>
2021-05-29 13:23:21 +1200

    createInfillLayer working now

--- Application/createInfillLayer.m
+++ Application/createInfillLayer.m
@@ -1,83 +1,75 @@
-fileLocation = "Test Objects/cubewithhole.STL";
+function [slicePath] = createInfillLayer(stl,sliceHeight,infillDistance)
+%     fileLocation = "Test Objects/cubewithhole.STL";
 
-% Inputs
-stl = importSTL(fileLocation);
-stl = stlTransform(stl, 90, 0, 0, 0, 0, 0, 100);
-sliceHeight = 0.2;
-sliceGen = FlatUniformSliceGenerator(stl,sliceHeight);
-layer = 50;
+    % Inputs
+%     stl = importSTL(fileLocation);
+%     stl = stlTransform(stl, 90, 0, 0, 0, 0, 0, 100);
 
-slicePath = sliceGen.getSlicePath;
-layerPath = sliceGen.slicePathLayer(layer);
+%     sliceHeight = 0.2;
+    sliceGen = FlatUniformSliceGenerator(stl,sliceHeight);
 
-% find how many parts the layerPath has, do a for loop for that.
-% for each part, generate a infill path by code below...
+    layer = 50;
 
-% Generate Polygon for Infill
+    % Set infill Distance
+%     infillDistance = 2;
 
-sliceSize = max(max(layerPath(:,1:2))); % Calculate max x and y
-t = 0:0.001:sliceSize;
+    % This will be handled by Module 2 GUI
+    slicePath = sliceGen.getSlicePath;
+    layerPath = sliceGen.slicePathLayer(layer);
 
-% f increases the size of the infill. size of gap = 1/f units.
-f = 2 ;
+    % Find minimum x and y values of layer path to set template of infill.
+    minX = min(layerPath(:,1));
+    maxX = max(layerPath(:,1));
+    minY = min(layerPath(:,2));
+    maxY = max(layerPath(:,2));
 
-sq = sliceSize*(square(pi*f*t)+1);
-infillPolygon = polyshape(t,sq);
-% plot(infillPolygon);
+    % Set polygon of layer
+    layerPolygon =  polyshape( layerPath(  : , 1 ),layerPath( :,2));
 
-numOfparts = max(layerPath(:,4));
-for i = 1: numOfparts
+    % Find all the points from min X to max X with incriment of infill
+    % distance.
+    res = linspace(minX,maxX,(maxX-minX)/infillDistance);
+    res = transpose(res);
+
+    % Pass back into a variable where intersect can understand.
+    minLineseg = zeros(height(res),1);
+    minLineseg(:,1) = minY;
+    maxLineseg = zeros(height(res),1);
+    maxLineseg(:,1) = maxY;
+
+    % Initialize Path Array 
+    %NVM I realised I wont know how many intersects it can be. :(
+    infillPath = [];
+    flip = 1;
+    % plot(layerPolygon);
+    for i=1:height(res)
+        % Create current line to cut the shape.
+        lineseg = [res(i,:) minLineseg(1,1); res(i,:) maxLineseg(1,1)];
+
+
+        [in,out] = intersect(layerPolygon,lineseg);
+
+        % Append to new array
+        if height(infillPath) == 0 && ~isempty(in)
+            infillPath = in;
+        elseif ~isempty(in)
+            infillPath(end+1,:) = infillPath(end,1:2);
+            % Flip flop to alternate to draw lines bottom and top.
+            if flip ==1
+                infillPath(end+1:(end)+height(in),1:2) = flipud(in(:,1:2));
+                flip = 2;
+            else
+                infillPath(end+1:(end)+height(in),1:2) = in(:,1:2);
+                flip = 1;
+            end
+%             plot(infillPath(:,1),infillPath(:,2));
+        end
+
+%         plot(in(:,1),in(:,2),'b');
+        hold on;
+    end
     
-    temp = find(ismember(layerPath(:,4),i,'rows'));
-    
-    layerPolygon =  polyshape( layerPath(  min(temp):max(temp) , 1 ),layerPath( min(temp):max(temp),2));
-    %     plot(layerPolygon);
-    [in, out] = intersect(layerPolygon,infillPolygon);
-    
-    inSort = sortrows(in.Vertices,1); % Sort infill path by x axis
-    plot(inSort(:,1),inSort(:,2)); % Plot infill sorted path.
-    
-    hold on;
-    
-    
-    %%%%%%%%%%% gap detection experiment %%%%%%%%%%%
-%     for j = 1:height(inSort)-1
-%     inc = 1;
-%     while inc <= height(inSort)-2
-%  
-%         %find how many points are on same x axis.
-%         idx = find(ismember(round(inSort(:,1),1), round(inSort(inc,1),1) ));
-%         
-%         if sum(idx) > 1
-%             for k = 1:height(idx)-2
-%                 pt1 = inSort(idx(2*k-1),1:2);
-%                 pt2 = inSort(idx(2*k),1:2);
-%                 plot( pt1 , pt2 );
-%                 hold on;
-%                 
-%             end
-%         inc = inc+2;
-%         else
-%             inc = inc + 1;
-%         end
-%         
-%               
-%     end
+    %%%%%%%%%% THIS IS ALL YOU NEED TO PLOT IT DANIEL%%%%%%%%%
+    plot(infillPath(:,1),infillPath(:,2));
 end
-
-
-
-
-
-
-
-hold on;
-
-
-
-
-
-% figure(2);
-% plot(in);
-
 
